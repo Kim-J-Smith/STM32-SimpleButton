15688c7959e4b324a32c3f09aaf8eeffe356dd50
Kim-J-Smith <3440910457@qq.com>
2025-10-08 23:44:17 +0800

    refactor : I rewrite this project base on https://github.com/Kim-J-Smith/Simple-Button, to make this project better

--- examples/example_combination_btn/example_combination_btn.h
+++
@@ -1,7 +0,0 @@
-# pragma once
-# include "kim_stm32_hal_button.h"
-
-KIM_BUTTON__DECLARE(button_0)
-KIM_BUTTON__DECLARE(button_1)
-KIM_BUTTON__DECLARE(button_2)
-KIM_BUTTON__DECLARE(button_3)
--- examples/example_multi_repeat/example_multi_repeat.h
+++
@@ -1,7 +0,0 @@
-# pragma once
-# include "kim_stm32_hal_button.h"
-
-// 进行按键的声明 Do button declaration
-KIM_BUTTON__DECLARE(myBtn0)
-
-
